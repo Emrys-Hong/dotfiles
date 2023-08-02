95f46b46c0fcc4f257a1fa4c980bd7a54f180c64
Emrys-Hong <hongpengfei.emrys@gmail.com>
2023-08-02 11:13:11 +0800

    Update

---
+++ scripts/git-editor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check the number of arguments
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <file>"
+  exit 1
+fi
+
+file=$1
+
+# Check the operating system and set the editor accordingly
+if [[ "$(uname)" == "Linux" ]]; then
+  editor="$HOME/.dotfiles/nvim/nvim.appimage"
+elif [[ "$(uname)" == "Darwin" ]]; then
+  editor="/usr/bin/vim"
+else
+  echo "Unsupported operating system."
+  exit 1
+fi
+
+# Set the editor for Git
+export GIT_EDITOR="$editor"
+
+# Run the chosen editor command with the provided file
+$GIT_EDITOR "$file"
+
