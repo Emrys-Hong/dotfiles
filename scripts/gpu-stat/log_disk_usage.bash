b8986e0dce2d60d63b069d74e34dfc6e06e73208
Emrys-Hong <hongpengfei.emrys@gmail.com>
2023-10-05 23:17:12 +0800

    Update

--- scripts/gpu-stat/log_disk_usage.bash
+++ scripts/gpu-stat/log_disk_usage.bash
@@ -2,8 +2,7 @@
 
 function dirs() {
     # Get the IP address of the machine using ifconfig and awk
-    ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
-    echo $ip
+    ip=$(/usr/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
 
     output_file="/home/emrys/.dotfiles/scripts/gpu-stat/disk_usage_${ip}.txt"
 
