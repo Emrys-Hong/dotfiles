6cd599532d8d14d0c544ca595b94fce5161c9cb2
Emrys-Hong <hongpengfei.emrys@gmail.com>
2023-10-09 16:52:48 +0800

    Update

--- scripts/gpu-stat/main.py
+++ scripts/gpu-stat/main.py
@@ -36,14 +36,16 @@ def parse_content(du_file):
 
 
 def main(ip):
+    csvs = {
+        ip: pd.read_csv(f"{ip}_gpu_log.csv", on_bad_lines="skip")
+        for ip in ip_list
+        if os.path.exists(f"{ip}_gpu_log.csv")
+    }
+    for k, v in csvs.items():
+        v["machine"] = k
+
     if ip == "Total":
-        df = pd.concat(
-            [
-                pd.read_csv(f"{ip}_gpu_log.csv", on_bad_lines="skip")
-                for ip in ip_list
-                if os.path.exists(f"{ip}_gpu_log.csv")
-            ]
-        )
+        df = pd.concat([csvs.values()])
         if per_user:
             user = st.selectbox("Select User: ", ["All User"] + list(set(df["user"])))
             if user != "All User":
@@ -51,17 +53,13 @@ def main(ip):
 
     else:
         if os.path.exists(f"{ip}_gpu_log.csv"):
-            df = pd.read_csv(f"{ip}_gpu_log.csv", on_bad_lines="skip")
+            df = csvs[ip]
         else:
-            df = pd.concat(
-                [
-                    pd.read_csv(f"{ip}_gpu_log.csv", on_bad_lines="skip")
-                    for ip in ip_list
-                    if os.path.exists(f"{ip}_gpu_log.csv")
-                ]
-            )
+            df = pd.concat([csvs.values()])
 
     if not per_user:
+        df["user"] = "All Users"
+    if per_machine:
         df["user"] = "All Users"
 
     df["datetime"] = pd.to_datetime(df["date"] + " " + df["time"])
@@ -72,15 +70,27 @@ def main(ip):
     truncated_time = now.replace(minute=0, second=0, microsecond=0)
     df = df[df["datetime"] < truncated_time]
 
-    grouped_df = (
-        df.groupby([pd.Grouper(key="datetime", freq="1H"), "user"])
-        .size()
-        .reset_index(name="count")
-    )
+    if per_machine:
+        grouped_df = (
+            df.groupby([pd.Grouper(key="datetime", freq="1H"), "machine"])
+            .size()
+            .reset_index(name="count")
+        )
 
-    table = grouped_df.pivot_table(
-        index="datetime", columns="user", values="count", fill_value=0
-    )
+        table = grouped_df.pivot_table(
+            index="datetime", columns="machine", values="count", fill_value=0
+        )
+
+    else:
+        grouped_df = (
+            df.groupby([pd.Grouper(key="datetime", freq="1H"), "user"])
+            .size()
+            .reset_index(name="count")
+        )
+
+        table = grouped_df.pivot_table(
+            index="datetime", columns="user", values="count", fill_value=0
+        )
     table = table.divide(12)  # one hour have 12 five minutes interval
 
     st.line_chart(table)
@@ -89,7 +99,7 @@ def main(ip):
         du_file = f"disk_usage_{ip}.txt"
         if os.path.exists(du_file):
             content = parse_content(du_file)
-            st.text_area("Disk Usage:", content, height=400)  # Adjust height as needed
+            st.text_area("Disk Usage:", content, height=800)  # Adjust height as needed
 
 
 if __name__ == "__main__":
@@ -99,6 +109,7 @@ if __name__ == "__main__":
     st.title("DeCLaRe Admin GPU & Disk Monitoring")
     chart_option = st.selectbox("Select Machine: ", ["Total"] + ip_list)
     per_user = st.checkbox("Show individual user usage", value=False)
+    per_machine = st.checkbox("Show individual machine usage", value=False)
     time_span = st.radio("Usage History in Days", ("1", "3", "7", "30"))
 
     main(chart_option)
